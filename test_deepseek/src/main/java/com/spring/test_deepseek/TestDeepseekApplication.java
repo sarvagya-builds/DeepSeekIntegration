244d241761632e59a8deb5f0a8a963810cc7b685
Kapilan <sriranjankapilan@gmail.com>
2025-02-23 04:03:13 +0530

    ollama dependency added and tested with ollama model

---
+++ test_deepseek/src/main/java/com/spring/test_deepseek/TestDeepseekApplication.java
@@ -0,0 +1,13 @@
+package com.spring.test_deepseek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDeepseekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestDeepseekApplication.class, args);
+    }
+
+}
---
+++ test_deepseek/src/test/java/com/spring/test_deepseek/TestDeepseekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.test_deepseek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDeepseekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
