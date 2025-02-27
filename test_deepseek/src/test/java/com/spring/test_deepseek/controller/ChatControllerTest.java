93325d18b3d4a21db7f197592e317f1b23daf7df
kapilan <k.sriranjan@homeit-system.de>
2025-02-27 06:37:37 +0530

    test stage added in Jenkinsfile + wrong test cases added to check jenkins pipeline

---
+++ test_deepseek/src/test/java/com/spring/test_deepseek/controller/ChatControllerTest.java
@@ -0,0 +1,37 @@
+package com.spring.test_deepseek.controller;
+
+import com.spring.test_deepseek.service.ChatService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(MockitoExtension.class)
+class ChatControllerTest {
+
+  @InjectMocks
+  ChatController chatController;
+  @Mock
+  ChatService chatService;
+
+  @Test
+  void generate() {
+  }
+
+  @Test
+  void generateStream() {
+  }
+
+  @Test
+  void getResponseFromDeepSeekAI() {
+    String question = "What is your name?";
+    Mockito.when(chatService.getResponseFromDeepSeekAI("jenkins")).thenReturn(question);
+  }
+
+  @Test
+  void getResponseFromDeepSeekAIWithStream() {
+  }
+}
